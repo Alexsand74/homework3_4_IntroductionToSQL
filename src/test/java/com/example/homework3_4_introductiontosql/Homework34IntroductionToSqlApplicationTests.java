8bf1c333b1561d0653c2f2adf246ced61e2062ea
Alexsand74 <1974alexsand@gmail.com>
2023-10-16 19:42:35 +0300

    Homework 3.4 will be found here!

---
+++ src/test/java/com/example/homework3_4_introductiontosql/Homework34IntroductionToSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework3_4_introductiontosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework34IntroductionToSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
